3a0ee47338cc2514f7a9fdbe5e55d1953f32f858
ryotasuga <ryotasuga@example.com>
2022-05-10 01:01:02 +0000

    非ログイントップページ作成

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,11 @@ use App\Http\Controllers\WelcomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+
+//タスク管理システム
+Route::get('/',[AuthController::class,'index']);
+
+//テスト
 Route::get('/welcome',[WelcomeController::class,'index']);
 Route::get('/welcome/second',[WelcomeController::class,'second']);
 Route::get('/welcome/third',[WelcomeController::class,'third']);
